b2537dade18ac8086baa2377e42e03556c7b94e3
AlejandroProfesor <alejandro.gmartin@cesjuanpablosegundo.es>
2025-02-10 09:19:29 +0100

    Seguimiento Ratón Al Completo

---
+++ README.md
@@ -0,0 +1,7 @@
+# React + Vite
+
+Proyecto hecho con vite + react
+
+Es sobre un seguimiento del ratón
+
+metete en la carpeta para verlo
--- raton/src/App.jsx
+++ raton/src/App.jsx
@@ -1,12 +1,64 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
+const SeguimientoRaton = () => {
+  /* activado */
+  const [seguir, setseguimiento] = useState(false)
+  /* posición */
+  const [posicion, setPosicion] = useState({ x: 250 , y: 250})
 
+  useEffect(() => {
+    /* console.log('efecto', { seguir }) */
+    const manejarMovimiento = (event) => {
+      /* Client  x y CLient Y tienen que llamarse así para coger las variables del objeto event de javascript */
+      const { clientX, clientY} = event
+      /* console.log(event)
+      console.log('me estoy moviento por las coordenadas', { clientX, clientY }) */
+      setPosicion({ x: clientX, y: clientY})
+    }
+
+    if (seguir) {
+      window.addEventListener('pointermove', manejarMovimiento)
+    }
+    return () => {
+      console.log('se reseteó')
+      window.removeEventListener('pointermove', manejarMovimiento)
+    }
+  }, [seguir])
+
+  return (
+     <>
+      {/* Circulo */}
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${posicion.x}px, ${posicion.y}px)`
+      }}
+      />
+      {/* Botón */}
+      <button onClick={() => setseguimiento(!seguir)}>
+        {seguir ? 'Desactivar' : 'Activar' } seguimiento
+      </button>
+     </>
+  )
+}
+
+function App () {
 
   return (
     <>
-      
+      <h3>Raton sigueme</h3>
+      <main>
+        <SeguimientoRaton></SeguimientoRaton>
+      </main>
     </>
   )
 }
